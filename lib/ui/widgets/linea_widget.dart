74d50235e46dccc23782b44e027f88562cb6c2be
skintigth <skintigthvader@gmail.com>
2019-02-02 11:19:50 -0600

    fixed some project local files imports to respect Dart documentation using relative path

--- lib/ui/widgets/linea_widget.dart
+++ lib/ui/widgets/linea_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:metroapp/models/index.dart';
+import '../../models/index.dart';
 import '../screens/linea_screen.dart';
 import 'estacion_widget.dart';
 
