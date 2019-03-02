b7e2804dbcba2e03a2d07dad5feeadb31aa73cdd
skintigth <skintigthvader@gmail.com>
2019-03-02 14:13:00 -0600

    Added hero animation while open to the icon in the center of the screen

--- lib/ui/screens/search_screen.dart
+++ lib/ui/screens/search_screen.dart
@@ -4,19 +4,16 @@ import '../../models/superestacion.dart';
 import '../screens/estacion_screen.dart';
 import '../widgets/estacion_info_widget.dart';
 
-class SearchScreen extends StatefulWidget{
-
+class SearchScreen extends StatefulWidget {
   final List<SuperEstacion> estaciones;
 
   SearchScreen({this.estaciones});
 
   @override
   _SearchScreenState createState() => _SearchScreenState();
-
 }
 
-class _SearchScreenState extends State<SearchScreen>{
-
+class _SearchScreenState extends State<SearchScreen> {
   List<SuperEstacion> nombresFiltrados = [];
   bool noHayBusqueda = true;
 
@@ -26,55 +23,76 @@ class _SearchScreenState extends State<SearchScreen>{
       appBar: AppBar(
         automaticallyImplyLeading: true,
         title: TextField(
-          autofocus: true,
-          style: TextStyle(color: Colors.white, fontSize: 20.0,),
-          decoration: InputDecoration(
-            //ToDo: Este texto será traducido
-            hintText: 'Nombre de la Estacion',
-            hintStyle: TextStyle(color: Colors.white70),
-          ),
-          onChanged: (busqueda){
-            setState(() {
-              if(busqueda.isNotEmpty){
-                noHayBusqueda = false;
-                nombresFiltrados.clear();
-                for (var estacion in widget.estaciones){
-                  if (estacion.nombre.toLowerCase().replaceAll('á', 'a')
-                      .replaceAll('é', 'e').replaceAll('í', 'i')
-                      .replaceAll('ó', 'o').replaceAll('ú', 'u')
-                      .contains(busqueda.toLowerCase()) || estacion.nombre.toLowerCase().contains(busqueda.toLowerCase())){
-                    nombresFiltrados.add(estacion);
+            autofocus: true,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+            decoration: InputDecoration(
+              //ToDo: Este texto será traducido
+              hintText: 'Nombre de la Estacion',
+              hintStyle: TextStyle(color: Colors.white70),
+            ),
+            onChanged: (busqueda) {
+              setState(() {
+                if (busqueda.isNotEmpty) {
+                  noHayBusqueda = false;
+                  nombresFiltrados.clear();
+                  for (var estacion in widget.estaciones) {
+                    if (estacion.nombre
+                            .toLowerCase()
+                            .replaceAll('á', 'a')
+                            .replaceAll('é', 'e')
+                            .replaceAll('í', 'i')
+                            .replaceAll('ó', 'o')
+                            .replaceAll('ú', 'u')
+                            .contains(busqueda.toLowerCase()) ||
+                        estacion.nombre
+                            .toLowerCase()
+                            .contains(busqueda.toLowerCase())) {
+                      nombresFiltrados.add(estacion);
+                    }
                   }
+                } else {
+                  nombresFiltrados.clear();
+                  noHayBusqueda = true;
                 }
-              }else{
-                nombresFiltrados.clear();
-                noHayBusqueda = true;
-              }
-            });
-          }
-        ),
+              });
+            }),
       ),
-      body: resultadosBusqueda(),
+      body: Hero(
+        tag: 'BotonBuscar',
+        child: resultadosBusqueda(),
+      ),
     );
   }
 
   //Cambia el body del scaffold segun haya o no resultados de busqueda
   Widget resultadosBusqueda() {
     //Si no hay busqueda muestra un icono de una lupa con una texto
-    if (noHayBusqueda){
-      return mensajeBusqueda(CommunityMaterialIcons.map_search_outline, 'Solo Escribe el Nombre de la Estacion');
-    }else{
+    if (noHayBusqueda) {
+      return mensajeBusqueda(CommunityMaterialIcons.map_search_outline,
+          'Solo Escribe el Nombre de la Estacion');
+    } else {
       // Si no hay resultados muestra un icono y un mensaje indicando que no hubo resultados
-      if (nombresFiltrados.isEmpty){
-        return mensajeBusqueda(CommunityMaterialIcons.emoticon_sad, 'Lo sentimos, no pudimos encontrar la estacion que buscas');
-      }else{
+      if (nombresFiltrados.isEmpty) {
+        return mensajeBusqueda(CommunityMaterialIcons.emoticon_sad,
+            'Lo sentimos, no pudimos encontrar la estacion que buscas');
+      } else {
         //Si hay resultados muestra la lista de resultados
         return ListView.builder(
           itemCount: nombresFiltrados.length,
-          itemBuilder: (context, index){
+          itemBuilder: (context, index) {
             return InkWell(
-              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => EstacionScreen(estacion: nombresFiltrados[index],))),
-              child: EstacionInfoWidget(estacion: nombresFiltrados[index],),
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => EstacionScreen(
+                            estacion: nombresFiltrados[index],
+                          ))),
+              child: EstacionInfoWidget(
+                estacion: nombresFiltrados[index],
+              ),
             );
           },
         );
@@ -82,7 +100,7 @@ class _SearchScreenState extends State<SearchScreen>{
     }
   }
 
-  Widget mensajeBusqueda(IconData icono, String mensaje){
+  Widget mensajeBusqueda(IconData icono, String mensaje) {
     return Center(
       child: Container(
         margin: EdgeInsets.symmetric(horizontal: 20.0),
@@ -106,5 +124,4 @@ class _SearchScreenState extends State<SearchScreen>{
       ),
     );
   }
-
 }
