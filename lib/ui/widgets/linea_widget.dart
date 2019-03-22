a98e43848de1ce6de52271f66d57ffe5dae37bfc
skintigth <skintigthvader@gmail.com>
2019-03-22 16:08:51 -0600

    changed SizedBox for Margin

--- lib/ui/widgets/linea_widget.dart
+++ lib/ui/widgets/linea_widget.dart
@@ -3,29 +3,44 @@ import '../../models/index.dart';
 import '../screens/linea_screen.dart';
 import 'estacion_widget.dart';
 
-class LineaWidget extends StatelessWidget{
-
+class LineaWidget extends StatelessWidget {
   final Linea linea;
 
-  LineaWidget(this.linea);
+  LineaWidget({@required this.linea});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
       width: MediaQuery.of(context).size.width,
       child: Column(
         children: <Widget>[
-          SizedBox(height: 10,),
+          // SizedBox(
+          //   height: 10,
+          // ),
           Padding(
             padding: EdgeInsets.only(right: 8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text(linea.nombre, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, color: Color(linea.color)),),
+                Text(
+                  linea.nombre,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(linea.color)),
+                ),
                 InkWell(
-                  child: Icon(Icons.arrow_forward, color: Color(linea.color),),
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => LineaScreen(linea: linea,))),
+                  child: Icon(
+                    Icons.arrow_forward,
+                    color: Color(linea.color),
+                  ),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => LineaScreen(
+                                linea: linea,
+                              ))),
                 ),
               ],
             ),
@@ -35,15 +50,18 @@ class LineaWidget extends StatelessWidget{
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
               itemCount: linea.numeroEstaciones,
-              itemBuilder: (context, index){
-                return EstacionWidget(linea.estaciones[index],);
+              itemBuilder: (context, index) {
+                return EstacionWidget(
+                  linea.estaciones[index],
+                );
               },
             ),
           ),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 10,
+          ),
         ],
       ),
     );
   }
-
 }
