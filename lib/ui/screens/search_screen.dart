7dfdcb649cc366aa768e4c530816b35ec4903cff
skintigth <skintigthvader@gmail.com>
2019-03-22 15:10:08 -0600

    Refactored "resultados_busqueda" in its own file

--- lib/ui/screens/search_screen.dart
+++ lib/ui/screens/search_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:community_material_icon/community_material_icon.dart';
 import '../../models/superestacion.dart';
-import '../screens/estacion_screen.dart';
-import '../widgets/estacion_info_widget.dart';
-import '../widgets/mensaje_con_icono_widget.dart';
+import '../widgets/resultados_busqueda_estaciones_widget.dart';
 
 class SearchScreen extends StatefulWidget {
   final List<SuperEstacion> estaciones;
@@ -66,48 +63,5 @@ class _SearchScreenState extends State<SearchScreen> {
         nombresFiltrados: nombresFiltrados,
       ),
     );
-  }
-}
-
-class ResultadosBusqueda extends StatelessWidget {
-  final bool noHayBusqueda;
-  final List<SuperEstacion> nombresFiltrados;
-
-  ResultadosBusqueda(
-      {@required this.noHayBusqueda, @required this.nombresFiltrados});
-
-  @override
-  Widget build(BuildContext context) {
-    if (noHayBusqueda) {
-      return MensajeConIcono(
-          icono: CommunityMaterialIcons.map_search_outline,
-          mensaje: 'Solo Escribe el Nombre de la Estacion');
-    } else {
-      // Si no hay resultados muestra un icono y un mensaje indicando que no hubo resultados
-      if (nombresFiltrados.isEmpty) {
-        return MensajeConIcono(
-            icono: CommunityMaterialIcons.emoticon_sad,
-            mensaje:
-                'Lo sentimos, no pudimos encontrar la estacion que buscas');
-      } else {
-        //Si hay resultados muestra la lista de resultados
-        return ListView.builder(
-          itemCount: nombresFiltrados.length,
-          itemBuilder: (context, index) {
-            return InkWell(
-              onTap: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => EstacionScreen(
-                            estacion: nombresFiltrados[index],
-                          ))),
-              child: EstacionInfoWidget(
-                estacion: nombresFiltrados[index],
-              ),
-            );
-          },
-        );
-      }
-    }
   }
 }
---
+++ lib/ui/widgets/resultados_busqueda_estaciones_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:community_material_icon/community_material_icon.dart';
+import '../../models/superestacion.dart';
+import '../screens/estacion_screen.dart';
+import '../widgets/mensaje_con_icono_widget.dart';
+import '../widgets/estacion_info_widget.dart';
+
+class ResultadosBusqueda extends StatelessWidget {
+  final bool noHayBusqueda;
+  final List<SuperEstacion> nombresFiltrados;
+
+  ResultadosBusqueda(
+      {@required this.noHayBusqueda, @required this.nombresFiltrados});
+
+  @override
+  Widget build(BuildContext context) {
+    if (noHayBusqueda) {
+      return MensajeConIcono(
+          icono: CommunityMaterialIcons.map_search_outline,
+          mensaje: 'Solo Escribe el Nombre de la Estacion');
+    } else {
+      // Si no hay resultados muestra un icono y un mensaje indicando que no hubo resultados
+      if (nombresFiltrados.isEmpty) {
+        return MensajeConIcono(
+            icono: CommunityMaterialIcons.emoticon_sad,
+            mensaje:
+                'Lo sentimos, no pudimos encontrar la estacion que buscas');
+      } else {
+        //Si hay resultados muestra la lista de resultados
+        return ListView.builder(
+          itemCount: nombresFiltrados.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => EstacionScreen(
+                            estacion: nombresFiltrados[index],
+                          ))),
+              child: EstacionInfoWidget(
+                estacion: nombresFiltrados[index],
+              ),
+            );
+          },
+        );
+      }
+    }
+  }
+}
